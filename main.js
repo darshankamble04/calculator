b7fe969133192683561b574ff7aefdba837f507d
DARSHAN SHARAD KAMBLE <84654898+darshankamble04@users.noreply.github.com>
2021-07-12 00:38:30 +0530

    cal

--- main.js
+++ main.js
@@ -1,37 +1,54 @@
 let expj = document.getElementById("exp");
 let ansj = document.getElementById("ans");
 let expVal = "";
+let mirrorVal = "";
 let buttons = document.querySelectorAll("button");
 for (ele of buttons) {
-  //   console.log(ele);
-  ele.addEventListener("click", (e) => {
-    let keyVal = e.target.innerHTML;
-    let keyVal2 = keyVal;
+    console.log(ele);
+    ele.addEventListener("click", (e) => {
+        let keyVal = e.target.innerHTML;
+        let keyVal2 = keyVal;
+        console.log(keyVal);
+        if (keyVal == "=") {
+            ansj.value = eval(expVal);
+        } else if (keyVal == "C") {
+            expVal = "";
+            ansj.value = "";
+        } else if (keyVal == "×") {
+            expVal += "*";
+            console.log("× is print");
+        } else if (keyVal == "÷") {
+            expVal += "/";
+        } else if (keyVal == "DEL") {
+            expVal = expVal.slice(0, -1);
+        } else {
+            expVal += keyVal;
+        }
+        expj.value = expVal;
+    });
+}
 
+window.addEventListener("keydown", (e2) => {
+    let keyVal = e2.key;
+    // console.log(e2);
     console.log(keyVal);
-    if (keyVal == "=") {
-      ansj.value = eval(expVal);
-    } else if (keyVal == "C") {
-      expVal = "";
-      ansj.value = "";
+    if (keyVal == "=" || keyVal == "Enter") {
+        ansj.value = eval(expVal);
+    } else if (keyVal == "Delete") {
+        expVal = "";
+        ansj.value = "";
+    } else if (keyVal == "*") {
+        expVal += "*";
+        console.log("× is print");
     } else if (keyVal == "×") {
-      expVal += "*";
-      console.log("× is print");
-    } else if (keyVal == "×") {
-      expVal += "*";
-      console.log("× is print");
+        expVal += "*";
+        console.log("× is print");
     } else if (keyVal == "÷") {
-      expVal += "/";
-    } else if (keyVal == "DEL") {
-      expVal = expVal.slice(0, -1);
+        expVal += "/";
+    } else if (keyVal == "Backspacs") {
+        expVal = expVal.slice(0, -1);
     } else {
-      expVal += keyVal;
+        expVal += keyVal;
     }
     expj.value = expVal;
-    //     if (keyVal.endsWith("=")) {
-    //       keyVal += "";
-    //       console.log("nhsbxj");
-    //       expVal = ansj.value;
-    //     }
-  });
-}
+});
