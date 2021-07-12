0c2c2064b460759cae7e9c53898e4357c8ae79b5
DARSHAN SHARAD KAMBLE <84654898+darshankamble04@users.noreply.github.com>
2021-07-12 13:35:34 +0530

    updating calculator

--- main.js
+++ main.js
@@ -1,66 +1,73 @@
 let expj = document.getElementById("exp");
 let ansj = document.getElementById("ans");
 let expVal = "";
+let mirrorVal = "";
 let buttons = document.querySelectorAll("button");
 for (ele of buttons) {
-    console.log(ele);
     ele.addEventListener("click", (e) => {
         let keyVal = e.target.innerHTML;
         let keyVal2 = keyVal;
         console.log(keyVal);
         if (keyVal == "=") {
             ansj.value = eval(expVal);
+            mirrorVal += "";
         } else if (keyVal == "C") {
             expVal = "";
+            mirrorVal = "";
             ansj.value = "";
         } else if (keyVal == "×") {
             expVal += "*";
+            mirrorVal += keyVal;
             console.log("× is print");
         } else if (keyVal == "÷") {
             expVal += "/";
+            mirrorVal += keyVal;
         } else if (keyVal == "DEL") {
             expVal = expVal.slice(0, -1);
+            mirrorVal = mirrorVal.slice(0, -1);
         } else {
             expVal += keyVal;
+            mirrorVal += keyVal;
         }
-        expj.value = expVal;
+
+        expj.value = mirrorVal;
     });
 }
 
 window.addEventListener("keydown", (e2) => {
     let keyVal = e2.key;
-    // console.log(e2);
     console.log(keyVal);
     if (keyVal == "=" || keyVal == "Enter") {
         ansj.value = eval(expVal);
     } else if (keyVal == "Delete") {
         expVal = "";
+        mirrorVal = "";
         ansj.value = "";
     } else if (keyVal == "*") {
         expVal += "*";
-        console.log("× is print");
-    } else if (keyVal == "×") {
-        expVal += "*";
+        mirrorVal += "×";
         console.log("× is print");
     } else if (keyVal == "÷") {
         expVal += "/";
+        mirrorVal = keyVal;
     } else if (keyVal == "Backspace") {
         expVal = expVal.slice(0, -1);
+        mirrorVal = mirrorVal.slice(0, -1);
     } else {
         expVal += keyVal;
+        mirrorVal += keyVal;
     }
-    expj.value = expVal;
-    // keyVal =;
+
+    expj.value = mirrorVal;
 });
-
-// setInterval(() => {
-//     let chan = Number(expj);
-//     ansj.value = eval(chan);
-//     // console.log(chan);
-//     if (ansj.value == "undefined") {
-//         ansj.value = "0";
-//     }
-
-//     // console.log(chan);
-//     if (chan == Number) {}
-// }, 1);
+setInterval(() => {
+    try {
+        ansj.value = eval(expVal);
+    } catch (error) {
+        ansj.value += "";
+    }
+    if (ansj.value == "undefined") {
+        // console.log("und");
+        ansj.value = 0;
+    }
+}, 0.1);
